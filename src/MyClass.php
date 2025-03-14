3fbd1ee7075cd3ffa5260382a18d2f402a84c0fd
Md Mahbub Alam Khan <hkhan.cse@gmail.com>
2025-03-14 16:16:28 +0200

    🚀 First commit.

---
+++ src/MyClass.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace BwlPlugin\HelloWorldPackage;
+
+/**
+ * Class MyClass
+ * 
+ * @package BwlPlugin\HelloWorldPackage
+ * @version 1.0.0
+ * @author Bwl<hkhan.cse@gmail.com>
+ * @category BwlPlugin
+ * @license http://opensource.org/licenses/MIT The MIT License (MIT)
+ */
+class MyClass
+{
+  public function sayHello()
+  {
+    return "Hello from MyClass!";
+  }
+}
